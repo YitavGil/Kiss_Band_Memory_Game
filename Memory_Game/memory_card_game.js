32b8ba5f5f897c1d0ac79829778cca993b34b549
Yitavg <yitavga@yahoo.com>
2024-09-04 11:26:54 +0300

    fixed "isMatch" logic. fixed buttons css

--- Memory_Game/memory_card_game.js
+++ Memory_Game/memory_card_game.js
@@ -1,32 +1,29 @@
 const cards = document.querySelectorAll('.cards-container');
-let score = document.querySelectorAll('user-score');
+let score = document.querySelector('#score-value');
 let userScore = 0;
 let winGame = document.querySelector(".win")
-let reset = document.querySelector('.reset-btn')
+let reset = document.querySelector('.reset')
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let openCards = 0;
 
-
 function flipCard() {
     if (lockBoard) return;
     if(this === firstCard) return;
     
-    this.classList.toggle('flip');
+    this.classList.add('flip');
 
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-       
         return;
     }
 
-        hasFlippedCard = false;
-        secondCard = this;
+    hasFlippedCard = false;
+    secondCard = this;
 
-        checkMatch();
-    
+    checkMatch();
 }
 
 function checkMatch() {
@@ -42,10 +39,8 @@ function checkMatch() {
             winGame.innerHTML = "<h2> You Win! </h2>" 
         }
     }
-    if(userScore > 6) {
-        score.textContent++ 
-    }
 }
+
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
@@ -59,45 +54,54 @@ function unflipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-        lockBoard = false;
-       }, 1200)
+        resetBoard();
+    }, 1200);
 }
 
 function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-    for(let i = 0; i<cards.length; i++) {
-        cards[i].classList.remove('flip');
-    }
-     openCards = 0;
-     userScore = 0;
 }   
 
-(function shuffle() {
+function resetGame() {
+    setTimeout(() => {
+        cards.forEach(card => {
+            card.classList.remove('flip');
+            card.addEventListener('click', flipCard);
+        });
+        shuffle();
+        openCards = 0;
+        userScore = 0;
+        score.textContent = userScore;
+        winGame.innerHTML = "";
+    }, 1000);
+}
+
+function shuffle() {
     cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() * 12);
-      card.style.order = randomPos;
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
     });
-  })();
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+reset.addEventListener('click', resetGame);
 
+let myAudio = document.querySelector(".play");
+myAudio.addEventListener("click", togglePlay)
 
- let myAudio = document.querySelector(".play");
-     myAudio.addEventListener("click", togglePlay)
+let audio = new Audio("./sound/kiss.mp3")
+let isPlaying = false;
+function togglePlay() {
+    if (isPlaying) {
+        audio.pause();
+        isPlaying = false
+        myAudio.textContent = "Play"
+    } else {
+        audio.play();
+        isPlaying = true;
+        myAudio.textContent = "Pause"
+    }
+}
 
-   let audio = new Audio("sound/kiss.mp3")
-    let isPlaying = false;
-    function togglePlay() {
-        if (isPlaying === true) {
-            audio.pause();
-            isPlaying = false
-            myAudio.textContent = "Play"
-        }
-        else {
-            audio.play();
-            isPlaying = true;
-            myAudio.textContent = "Pause"
-        }
-    };
-
+shuffle();
